de4f7a3b06617ae47f47664f4fb3bb707fe155f8
JayVora-SerpentCS <k.mirza.serpentcs@gmail.com>
2014-07-09 16:12:29 +0530

    [ADD] report_extended : Add Module Report_Extended Which Add  Report Header With Company Address

---
+++ report_extended/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Report Extended',
+    'category': 'Base',
+    'summary': 'Report',
+    'version': '0.01',
+    'description': """
+        Report Extended Which Insert The Header Of Company's Full Address In Qweb Report
+            """,
+    'author': 'OpenERP SA',
+    'depends': ['report'],
+    'data': [
+        'views/layouts.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
