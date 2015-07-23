38a1ee3afa63b64255e92265cb0c307c38f1c2ee
JayVora-SerpentCS <jay.vora@serpentcs.com>
2015-07-23 20:59:13 +0530

    Renamed module for odoo apps as the name was conflicting

---
+++ hotel_report_layout/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Report Extended',
+    'category': 'Base',
+    'summary': 'Report',
+    'version': '0.01',
+    'description': """
+        Report Extended Which Insert The Header Of Company's Full Address In Qweb Report
+            """,
+    "author": "Serpent Consulting Services Pvt. Ltd., OpenERP SA",
+    "website": "http://www.serpentcs.com",
+    'depends': ['report'],
+    'data': [
+        'views/layouts.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
