f3d932eeb825b85dbe4fa226c31b3c15b8538783
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2016-11-28 12:09:39 +0530

    [FIX]Fix the wrong version format error in Hotel Module in HMS

--- hotel/__manifest__.py
+++ hotel/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Hotel Management',
-    'version': '1.0',
+    'version': '10.0.0.0.0',
     'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA,\
     Odoo Community Association (OCA),',
     'images': [],
