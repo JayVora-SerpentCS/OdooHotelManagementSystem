654c582fe3616d49784b01c1e97446a1c3e065c8
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-04-04 19:34:49 +0530

    [ADD]Added pos_order_for_restaurant module.

--- pos_order_for_restaurant/__manifest__.py
+++ pos_order_for_restaurant/__manifest__.py
@@ -2,24 +2,26 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Pos Receipt',
+    'name': 'Parcel/Delivery Order For Restaurant',
     'version': '10.0.1.0.0',
     'category': 'Point Of Sale',
     'sequence': 6,
-    'summary': 'Touchscreen Interface for Shops',
+    'summary': 'Touchscreen Interface for Restaurant',
     'author': 'Serpent Consulting Services Pvt. Ltd.,\
     Odoo Community Association (OCA)',
-    'website': 'http://www.serpentcs.com',
     'license': 'AGPL-3',
+    'website': 'http://www.serpentcs.com',
     'installable': True,
     'application': True,
-    'data':['security/pos_receipt_security.xml',
+    'data': ['security/pos_order_for_restaurant_security.xml',
             'security/ir.model.access.csv',
             'view/templates.xml',
-            'view/pos_receipt_view.xml',
-            'view/kitchen_screen_data.xml',
+            'view/pos_order_for_restaurant_view.xml',
            ],
-    'depends': ['point_of_sale', 'pos_options_bar'],
-    'qweb': ['static/src/xml/pos_receipt.xml'],
+    'demo': [
+        'view/pos_order_restaurant_demo.xml',
+     ],
+    'depends': ['point_of_sale', 'pos_restaurant','pos_receipt'],
+    'qweb': ['static/src/xml/pos_order_for_restaurant.xml'],
     'auto_install': False,
 }
