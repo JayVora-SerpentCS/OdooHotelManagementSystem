51a1a24a9757b8bc5480cc5274520273870243c3
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-04-05 17:18:49 +0530

    [IMP]Improved report_hotel_restaurant and board_frontdesk module.

--- board_frontdesk/__manifest__.py
+++ board_frontdesk/__manifest__.py
@@ -1,40 +1,20 @@
 # -*- coding: utf-8 -*-
-# --------------------------------------------------------------------------
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012-Today Serpent Consulting Services PVT. LTD.
-#    (<http://www.serpentcs.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-# ---------------------------------------------------------------------------
+# See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "Board for Hotel FrontDesk",
-    "version": "10.0.1.0.0",
-    "author": "Serpent Consulting Services Pvt. Ltd., OpenERP SA,\
-    Odoo Community Association (OCA)",
-    "website": "http://www.serpentcs.com",
-    "category": "Board/Hotel FrontDesk",
-    "depends": [
-        "board",
-        "report_hotel_restaurant",
-        "hotel_pos_restaurant"
+    'name': 'Board for Hotel FrontDesk',
+    'version': '10.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA',
+    'website': 'http://www.serpentcs.com',
+    'category': 'Board/Hotel FrontDesk',
+    'depends': [
+        'board',
+        'report_hotel_restaurant',
+        'hotel_pos_restaurant'
         ],
-    'license': "AGPL-3",
-    "data": [
-        "views/board_frontdesk_view.xml"
+    'license': 'AGPL-3',
+    'data': [
+        'views/board_frontdesk_view.xml'
     ],
-    "installable": True,
+    'installable': True,
 }
