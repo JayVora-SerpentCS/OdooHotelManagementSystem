5808d3b27d240d29c62f2c27e6c375f0e508a1e2
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-04-05 17:00:37 +0530

    [IMP]Improved report_hotel_reservation module.

--- report_hotel_reservation/__manifest__.py
+++ report_hotel_reservation/__manifest__.py
@@ -1,37 +1,17 @@
 # -*- coding: utf-8 -*-
-# --------------------------------------------------------------------------
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012-Today Serpent Consulting Services PVT. LTD.
-#    (<http://www.serpentcs.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-# ---------------------------------------------------------------------------
+# See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "Hotel Reservation Management - Reporting",
-    "version": "10.0.1.0.0",
-    "author": "Serpent Consulting Services Pvt. Ltd., OpenERP SA,\
-    Odoo Community Association (OCA)",
-    "website": "http://www.serpentcs.com",
-    "depends": ["hotel_reservation"],
-    'license': "AGPL-3",
-    "category": "Generic Modules/Hotel Reservation",
-    "data": [
-        "security/ir.model.access.csv",
-        "views/report_hotel_reservation_view.xml",
+    'name': 'Hotel Reservation Management - Reporting',
+    'version': '10.0.1.0.0',
+    'author': 'Serpent Consulting Services Pvt. Ltd., OpenERP SA',
+    'website': 'http://www.serpentcs.com',
+    'depends': ['hotel_reservation'],
+    'license': 'AGPL-3',
+    'category': 'Generic Modules/Hotel Reservation',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/report_hotel_reservation_view.xml',
     ],
     'installable': True,
     'auto_install': False,
