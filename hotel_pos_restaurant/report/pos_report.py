0f1c0e6d0236a6ca6b6535d28b800b2d19583458
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-04-05 16:23:39 +0530

    [IMP]Improved hotel_pos_restaurant module.

--- hotel_pos_restaurant/report/pos_report.py
+++ hotel_pos_restaurant/report/pos_report.py
@@ -1,25 +1,5 @@
 # -*- coding: utf-8 -*-
-#############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2012-Today Serpent Consulting Services Pvt. Ltd.
-#    (<http://www.serpentcs.com>)
-#    Copyright (C) 2004 OpenERP SA (<http://www.openerp.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>
-#
-#############################################################################
+# See LICENSE file for full copyright and licensing details.
 
 import time
 from datetime import datetime
@@ -75,7 +55,6 @@ class ReportLunchorder1(models.AbstractModel):
         rm_act = self.with_context(data['form'].get('used_context', {}))
         get_data = rm_act.get_data(date_start, date_end)
         get_pos = rm_act.get_pos(date_start, date_end)
-#       gettotal = rm_act.gettotal(pos_order)
         getTotal = rm_act.getTotal()
         docargs = {
             'doc_ids': docids,
