a0ff2cef9517d76d2e55adabdcc3c2ab17f9b837
Krishna Prajapati <krishna.prajapati.serpentcs@gmail.com>
2017-09-22 14:15:13 +0530

    [IMP]Improve code in hotel, hotel_reservaiton,hotel_restaurant and hotel_housekeeping as per requirements.

--- hotel_housekeeping/models/hotel_housekeeping.py
+++ hotel_housekeeping/models/hotel_housekeeping.py
@@ -7,22 +7,59 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
-class ProductCategory(models.Model):
-
-    _inherit = "product.category"
-
-    isactivitytype = fields.Boolean('Is Activity Type',
-                                    default=lambda *a: True)
-
-
 class HotelHousekeepingActivityType(models.Model):
 
     _name = 'hotel.housekeeping.activity.type'
     _description = 'Activity Type'
 
-    activity_id = fields.Many2one('product.category', 'Category',
-                                  required=True, delegate=True,
-                                  ondelete='cascade', index=True)
+    name = fields.Char('Name', size=64, required=True)
+    activity_id = fields.Many2one('hotel.housekeeping.activity.type',
+                                  'Category')
+
+    @api.multi
+    def name_get(self):
+        def get_names(cat):
+            """ Return the list [cat.name, cat.activity_id.name, ...] """
+            res = []
+            while cat:
+                res.append(cat.name)
+                cat = cat.activity_id
+            return res
+        return [(cat.id, " / ".join(reversed(get_names(cat)))) for cat in self]
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        if not args:
+            args = []
+        if name:
+            # Be sure name_search is symetric to name_get
+            category_names = name.split(' / ')
+            parents = list(category_names)
+            child = parents.pop()
+            domain = [('name', operator, child)]
+            if parents:
+                names_ids = self.name_search(' / '.join(parents), args=args,
+                                             operator='ilike', limit=limit)
+                category_ids = [name_id[0] for name_id in names_ids]
+                if operator in expression.NEGATIVE_TERM_OPERATORS:
+                    categories = self.search([('id', 'not in', category_ids)])
+                    domain = expression.OR([[('activity_id', 'in',
+                                              categories.ids)], domain])
+                else:
+                    domain = expression.AND([[('activity_id', 'in',
+                                               category_ids)], domain])
+                for i in range(1, len(category_names)):
+                    domain = [[('name', operator,
+                                ' / '.join(category_names[-1 - i:]))], domain]
+                    if operator in expression.NEGATIVE_TERM_OPERATORS:
+                        domain = expression.AND(domain)
+                    else:
+                        domain = expression.OR(domain)
+            categories = self.search(expression.AND([domain, args]),
+                                     limit=limit)
+        else:
+            categories = self.search(args, limit=limit)
+        return categories.name_get()
 
 
 class HotelActivity(models.Model):
@@ -32,6 +69,8 @@ class HotelActivity(models.Model):
 
     h_id = fields.Many2one('product.product', 'Product', required=True,
                            delegate=True, ondelete='cascade', index=True)
+    categ_id = fields.Many2one('hotel.housekeeping.activity.type',
+                               string='Category')
 
 
 class HotelHousekeeping(models.Model):
